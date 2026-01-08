c6bf5f7f8773397562b801fb9f1046658496677a
mspruc <mspruc@users.noreply.github.com>
2026-01-08 15:26:31 +0100

    add sql impl for reduce descriptors

--- wayang-commons/wayang-core/src/main/java/org/apache/wayang/core/function/ReduceDescriptor.java
+++ wayang-commons/wayang-core/src/main/java/org/apache/wayang/core/function/ReduceDescriptor.java
@@ -28,8 +28,10 @@ import org.apache.wayang.core.types.DataUnitType;
 import java.util.function.BinaryOperator;
 
 /**
- * This descriptor pertains to functions that take multiple data units and aggregate them into a single data unit
- * by means of a tree-like fold, i.e., using a commutative, associative function..
+ * This descriptor pertains to functions that take multiple data units and
+ * aggregate them into a single data unit
+ * by means of a tree-like fold, i.e., using a commutative, associative
+ * function..
  */
 public class ReduceDescriptor<Type> extends FunctionDescriptor {
 
@@ -39,18 +41,19 @@ public class ReduceDescriptor<Type> extends FunctionDescriptor {
 
     private final SerializableBinaryOperator<Type> javaImplementation;
 
+    private String sqlImplementation;
+
     public ReduceDescriptor(SerializableBinaryOperator<Type> javaImplementation,
-                            DataUnitGroupType<Type> inputType,
-                            BasicDataUnitType<Type> outputType) {
+            DataUnitGroupType<Type> inputType,
+            BasicDataUnitType<Type> outputType) {
         this(javaImplementation, inputType, outputType, new NestableLoadProfileEstimator(
                 LoadEstimator.createFallback(1, 1),
-                LoadEstimator.createFallback(1, 1)
-        ));
+                LoadEstimator.createFallback(1, 1)));
     }
 
     public ReduceDescriptor(SerializableBinaryOperator<Type> javaImplementation,
-                            DataUnitGroupType<Type> inputType, BasicDataUnitType<Type> outputType,
-                            LoadProfileEstimator loadProfileEstimator) {
+            DataUnitGroupType<Type> inputType, BasicDataUnitType<Type> outputType,
+            LoadProfileEstimator loadProfileEstimator) {
         super(loadProfileEstimator);
         this.inputType = inputType;
         this.outputType = outputType;
@@ -59,13 +62,12 @@ public class ReduceDescriptor<Type> extends FunctionDescriptor {
 
     public ReduceDescriptor(SerializableBinaryOperator<Type> javaImplementation, Class<Type> inputType) {
         this(javaImplementation, DataUnitType.createGroupedUnchecked(inputType),
-                DataUnitType.createBasicUnchecked(inputType)
-        );
+                DataUnitType.createBasicUnchecked(inputType));
     }
 
-
     /**
-     * This is function is not built to last. It is thought to help out devising programs while we are still figuring
+     * This is function is not built to last. It is thought to help out devising
+     * programs while we are still figuring
      * out how to express functions in a platform-independent way.
      *
      * @return a function that can perform the reduce
@@ -74,8 +76,18 @@ public class ReduceDescriptor<Type> extends FunctionDescriptor {
         return this.javaImplementation;
     }
 
+    public ReduceDescriptor<Type> withSqlImplementation(final String impl) {
+        this.sqlImplementation = impl;
+        return this;
+    }
+
+    public String getSqlImplementation() {
+        return this.sqlImplementation;
+    }
+
     /**
-     * In generic code, we do not have the type parameter values of operators, functions etc. This method avoids casting issues.
+     * In generic code, we do not have the type parameter values of operators,
+     * functions etc. This method avoids casting issues.
      *
      * @return this instance with type parameters set to {@link Object}
      */
